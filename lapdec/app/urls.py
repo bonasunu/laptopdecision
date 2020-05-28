0a7dba618edf0fc1f3ea5289f7817578ce60780f
bonasunu <bonaventurasunu@gmail.com>
2020-05-28 21:19:01 +0700

    add laptops, finder, comparison page

--- lapdec/app/urls.py
+++ lapdec/app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('laptops', views.laptops, name='laptops'),
+    path('finder', views.finder, name='finder'),
+    path('comparison', views.comparison, name='comparison'),
 ]
--- lapdec/app/views.py
+++ lapdec/app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'app/index.html')
+
+def laptops(request):
+    return render(request, 'app/laptops.html')
+
+def finder(request):
+    return render(request, 'app/finder.html')
+
+def comparison(request):
+    return render(request, 'app/comparison.html')
